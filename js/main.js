cfd4884c78a98834d21306f4211cfa05adcecd05
Sisiyao <hustgsy@126.com>
2018-11-27 19:44:27 +0800

    arcgis for javascript combining Echarts

---
+++ js/main.js
@@ -0,0 +1,41 @@
+﻿dojo.require("esri.map");
+dojo.require("esri.layers.ArcGISDynamicMapServiceLayer");
+dojo.require("src/Echarts3Layer");
+
+dojo.ready(function() {
+
+	var map = new esri.Map('map', {
+		basemap: 'osm',
+		center: [114.338119,30.508913],  //114.338119,30.508913
+		zoom: 12,
+		navigationMode: "css-transform",
+		force3DTransforms: true,
+		logo: false,
+		fitExtent: true,
+		fadeOnZoom: true,
+		slider: false
+	});
+
+	dojo.connect(map, 'onLoad', function(themap) {
+
+		var heatmap = Dcq.Heatmap.Instance();
+		if(!heatmap.isInited()) {
+			heatmap.init({
+				'map': themap,
+				'echarts': echarts
+			});
+		}
+
+		//开始获取数据
+		$.get("data_yuan2.json", function(data) {
+			var result = [];
+			for(var i = 0; i < data.length; i++) {
+				for(var j = 0; j < data[i].length; j++) {
+					var item = data[i][j];
+					result.push([item.coord[0], item.coord[1]]);
+				}
+			}
+			heatmap.drawChart(result);
+		}, "json");
+	});
+});
